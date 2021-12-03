39673f270d8ca4370a924ba81a6949205723808d
Abubakar Mahmood Garba <94145681+SadeeqMg01@users.noreply.github.com>
2021-12-03 09:32:47 +0100

    Update Script_Saleh.py

--- Script_Saleh.py
+++ Script_Saleh.py
@@ -1,8 +1,4 @@
-#<-----------Saleh Project------------>
-print("\t-------------BIOINFORMATICS SCRIPT---------------\n")
+#!/usr/bin/env bash
 
-print("\t\tHello Saleh salisu, below are your informations:\n")
-print("Name: " + "\t\t\t" + "Saleh Salihu")
-print("Email: " + "\t\t\t"+ "salehsalisu27@gmail.com")
-print("Slack_username: " + "\t" + "@Saleh_Salisu")
-print("Area of interest: " + "\t" + "Proteomics")
+
+echo "Saleh Salihu,salehsalisu6388@gmail.com,@Saleh_Salisu,Proteomics"
