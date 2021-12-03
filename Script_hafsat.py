210ba717a7ccb5ec77a8d5b0ad947b6d4d12e078
Abubakar Mahmood Garba <94145681+SadeeqMg01@users.noreply.github.com>
2021-12-03 09:35:41 +0100

    Update Script_hafsat.py

--- Script_hafsat.py
+++ Script_hafsat.py
@@ -1,8 +1,4 @@
-#<-----------Hafsat Project------------>
-print("\t-------------BIOINFORMATICS SCRIPT---------------\n")
+#!/usr/bin/env bash
 
-print("\t\tHello Hafsat Ahmad Haruna, below are your informations:\n")
-print("Name: " + "\t\t\t" + "Hafsat Ahmad Haruna")
-print("Email: " + "\t\t\t"+ "hafsatahmadharuna@gmail.com")
-print("Slack_username: " + "\t" + "@Hasat_Ahmad_Haruna")
-print("Area of interest: " + "\t" + "System biology")
+
+echo "Hafsat Ahmad Haruna,hafsatahmadharuna@gmail.com,@Hasat_Ahmad_Haruna,System biology"
