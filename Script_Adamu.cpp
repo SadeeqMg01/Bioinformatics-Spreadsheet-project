1250ee78f0e3c789376b5af431184e30100636ac
Abubakar Mahmood Garba <94145681+SadeeqMg01@users.noreply.github.com>
2021-12-03 09:11:40 +0100

    Update Script_Adamu.cpp

--- Script_Adamu.cpp
+++ Script_Adamu.cpp
@@ -1,13 +1,4 @@
-#include<iostream>
-using namespace std;
+#!/usr/bin/env bash
 
-int main()
-{
-    cout << "-------------------BIOINFORMATICS C++ SCRIPT----------------\n";
-    cout << "\t\tHello Adamu Aliyu Dahiru:\n\n";
-    cout << "Name:"<< "\t\t\t" << "Adamu Aliyu Dahiru \n";
-    cout << "Email:"<< "\t\t\t" << "aliyuadamudahiru3@gmail.com\n";
-    cout << "Slack_Id:" << "\t\t" << "@Aliyu_Adamu_Dahiru \n";
-    cout << "Area Of Interest:" << "\t" << "Gemomics \n";
-    return 0;
-}
+
+    echo "Adamu Aliyu Dahiru,aliyuadamudahiru3@gmail.com,@Aliyu_Adamu_Dahiru,Gemomics"
