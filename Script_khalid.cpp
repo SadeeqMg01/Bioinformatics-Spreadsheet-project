f29b944285e3a61cc62c9b9215f75b003614f305
Abubakar Mahmood Garba <94145681+SadeeqMg01@users.noreply.github.com>
2021-12-03 09:38:45 +0100

    Update Script_khalid.cpp

--- Script_khalid.cpp
+++ Script_khalid.cpp
@@ -1,13 +1,4 @@
-#include<iostream>
-using namespace std;
+#!/usr/bin/env bash
 
-int main()
-{
-    cout << "-------------------BIOINFORMATICS C++ SCRIPT----------------\n";
-    cout << "\t\tHello Khalid Ibrahim:\n\n";
-    cout << "Name:"<< "\t\t\t" << "Khalid Ibrahim \n";
-    cout << "Email:"<< "\t\t\t" << "khalidibrahimdut@gmail.com\n";
-    cout << "Slack_Id:" << "\t\t" << "@Khalid_Ibrahim \n";
-    cout << "Area Of Interest:" << "\t" << "Gemomics \n";
-    return 0;
-}
+    
+    echo "Khalid Ibrahim,khalidibrahimdut@gmail.com,@Khalid_Ibrahim,Gemomics"
