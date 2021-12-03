4208284c058136f6dba6ae38be1d44d81036a1f7
Abubakar Mahmood Garba <94145681+SadeeqMg01@users.noreply.github.com>
2021-12-03 09:18:23 +0100

    Update Script_Hauwau.cpp

--- Script_Hauwau.cpp
+++ Script_Hauwau.cpp
@@ -1,13 +1,2 @@
-#include<iostream>
-using namespace std;
-
-int main()
-{
-    cout << "-------------------BIOINFORMATICS C++ SCRIPT----------------\n";
-    cout << "\t\tHello Hauwau Musa:\n\n";
-    cout << "Name:"<< "\t\t\t" << "Hauwau Musa \n";
-    cout << "Email:"<< "\t\t\t" << "hm691541@gmail.com\n";
-    cout << "Slack_Id:" << "\t\t" << "@Aliyu_Adamu_Dahiru \n";
-    cout << "Area Of Interest:" << "\t" << "Proteomics \n";
-    return 0;
-}
+#!/usr/bin/env bash
+    echo "Hauwau Musa,hm691541@gmail.com,@Aliyu_Adamu_Dahiru,Proteomics"
