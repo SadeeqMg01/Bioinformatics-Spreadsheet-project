6f9c4f8243290656ee3b093a96d1810fa92d068b
Abubakar Mahmood Garba <94145681+SadeeqMg01@users.noreply.github.com>
2021-12-03 09:08:40 +0100

    Update Script_Abubakar.py

--- Script_Abubakar.py
+++ Script_Abubakar.py
@@ -1,8 +1,4 @@
-#<-----------Sadeeq Project------------>
-print("\t-------------BIOINFORMATICS SCRIPT---------------\n")
+#!/usr/bin/env bash
 
-print("\t\tHello Abubakar Mahmood Garba, below are your informations:\n")
-print("Name: " + "\t\t\t" + "Abubakar Mahmood Garba")
-print("Email: " + "\t\t\t"+ "abubakarmahmood01@gmail.com")
-print("Slack_username: " + "\t" + "@abubakar_m_garba")
-print("Area of interest: " + "\t" + "Genomics")
+
+echo "Abubakar Mahmood Garba,abubakarmahmood01@gmail.com,@abubakar_m_garba,Genomics"
